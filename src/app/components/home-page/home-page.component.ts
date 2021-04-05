874d195f7002f7a5d6bee36e7957729ab014b779
MugeraH <bwosihughes24@gmail.com>
2021-04-06 01:14:10 +0300

    add images to assets folder

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubDataService } from '../../services/github-data.service';
+import { Users } from '../../users';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
+  userData: Users[];
+  userRepoData: Users[];
+  searchTerm: string = 'hmugera';
+  constructor(private dataService: GithubDataService) {}
 
   ngOnInit(): void {
+     this.getUserData();
+    this.getUserRepoData();
   }
 
+  getUserData() {
+    this.dataService.getUserData(this.searchTerm).subscribe((data) => {
+      this.userData = data;
+
+      console.log(this.userData);
+    });
+  }
+  getUserRepoData() {
+    this.dataService.getRepoData(this.searchTerm).subscribe((data) => {
+      this.userRepoData = data;
+      console.log(this.userRepoData);
+    });
+  }
 }
